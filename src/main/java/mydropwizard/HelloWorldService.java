e59cbba38a55624246b9351281d19552c9e187bb
Rafael Portela <rafaelportela90@gmail.com>
2013-08-13 20:50:22 -0300

    hello-world service, resource, pojo and configuration.

---
+++ src/main/java/mydropwizard/HelloWorldService.java
@@ -0,0 +1,27 @@
+package mydropwizard;
+
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Environment;
+import mydropwizard.resources.HelloWorldResource;
+
+public class HelloWorldService extends Service<HelloWorldConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new HelloWorldService().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        bootstrap.setName("hello-world");
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration, Environment environment) throws Exception {
+
+        final String template = configuration.getTemplate();
+        final String defaultName = configuration.getDefaultName();
+
+        environment.addResource(new HelloWorldResource(template, defaultName));
+    }
+}
